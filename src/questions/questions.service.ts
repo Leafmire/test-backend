59d065369096299e5d1bafb8d1a8c0ae343f2362
Leafmire <ekwkqdk007@naver.com>
2024-05-31 18:24:08 +0900

    feat: question

--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -1,19 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { CreateQuestionDto } from './dto/create-question.dto';
 import { UpdateQuestionDto } from './dto/update-question.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class QuestionsService {
+  constructor(private prisma: PrismaService) {}
+
   create(createQuestionDto: CreateQuestionDto) {
-    return 'This action adds a new question';
+    return this.prisma.question.create({ data: createQuestionDto });
   }
 
   findAll() {
-    return `This action returns all questions`;
+    return this.prisma.question.findMany();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} question`;
+    return this.prisma.question.findUnique({ where: { question_id: id } });
   }
 
   update(id: number, updateQuestionDto: UpdateQuestionDto) {
